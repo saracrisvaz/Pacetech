ccc43d55cd8866decbba75057b20de7a0782c625
Sara Cristina Vaz <100101027+saracrisvaz@users.noreply.github.com>
2022-04-03 16:56:58 -0300

    Update CalculadoraPace1.java

--- CalculadoraPace1.java
+++ CalculadoraPace1.java
@@ -59,13 +59,12 @@ public class CalculadoraPace extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Exemplo().setVisible(true);
             }
         });
-    };looooot modify                     
+    }
     // End of variables declaration                   
 }
 
@@ -389,7 +388,6 @@ public class CalculadoraPace extends javax.swing.JFrame {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -408,7 +406,6 @@ public class CalculadoraPace extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new CalculadoraPace().setVisible(true);
